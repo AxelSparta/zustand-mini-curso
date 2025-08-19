1a7794f01b0c38050a7a971ab2222b7cc590c022
AxelSparta <axelnicolassparta@gmail.com>
2025-08-19 08:13:12 -0300

    add custom storage

--- src/store/person/person.store.ts
+++ src/store/person/person.store.ts
@@ -1,5 +1,5 @@
-import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { create, StateCreator } from 'zustand'
+import { createJSONStorage, persist, StateStorage } from 'zustand/middleware'
 
 interface PersonState {
   firstName: string
@@ -11,17 +11,32 @@ interface Actions {
   setLastName: (value: string) => void
 }
 
-export const usePersonStore = create<PersonState & Actions>()(
-  persist(
-    set => ({
-      firstName: '',
-      lastName: '',
+const customStorage: StateStorage = {
+  getItem: function (name: string): string | null | Promise<string | null> {
+    console.log(name, 'getItem called')
+    return null
+  },
+  setItem: function (name: string, value: string): unknown | Promise<unknown> {
+    console.log(name, 'setItem called with value:', value)
+    return null
+  },
+  removeItem: function (name: string): unknown | Promise<unknown> {
+    console.log(name, 'removeItem called')
+    return null
+  }
+}
 
-      setFirstName: (value: string) => set({ firstName: value }),
-      setLastName: (value: string) => set({ lastName: value })
-    }),
-    {
-      name: 'person-storage' // unique name
-    }
-  )
+const storeAPI: StateCreator<PersonState & Actions> = set => ({
+  firstName: '',
+  lastName: '',
+
+  setFirstName: (value: string) => set({ firstName: value }),
+  setLastName: (value: string) => set({ lastName: value })
+})
+
+export const usePersonStore = create<PersonState & Actions>()(
+  persist(storeAPI, {
+    name: 'person-storage', // unique name
+    storage: createJSONStorage(() => customStorage) // (optional) by default, 'localStorage' is used
+  })
 )
